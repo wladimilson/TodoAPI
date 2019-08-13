f1c77b94cb76b4a836feabb7be3f036387ba8d05
Wladimilson <9863709+wladimilson@users.noreply.github.com>
2019-08-13 14:24:41 -0300

    Atualização para o .NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  
               .AddJwtBearer(options => {  
